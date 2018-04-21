ec06e91cf0d99226a8c308391467e16a67e28b40
yuto.komai <yuto.komai@aktsk.jp>
2018-04-21 18:47:33 +0900

    :+1: add okay post method

--- line_messaging_api_test.rb
+++ line_messaging_api_test.rb
@@ -42,3 +42,23 @@ post '/callback' do
 
   "OK"
 end
+
+post '/okay' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    message = {
+        type: 'text',
+        text: '一緒に行こうね♡'
+    }
+    client.reply_message(event['replyToken'], message)
+  }
+
+  "OK"
+end
